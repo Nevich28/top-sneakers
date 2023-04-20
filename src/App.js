1e3bae5f647160e1d6e7fafc031187e8c9cef737
Andrey <andrey.liubashchenko@gmail.com>
2023-04-20 15:37:51 +0300

    before goods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import {SlBasket} from 'react-icons/sl';
+import {BiUserCircle} from 'react-icons/bi';
+import {BsPlus} from 'react-icons/bs';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="wrapper clear">
+			<header className='d-flex justify-between align-center p-40'>
+				<div className="d-flex align-center">
+					<img width={40} height={40} src="/img/logo.png" alt="logo"/>
+					<div>
+						<h3 className="text-uppercase">TOP SNEAKERS</h3>
+						<p>Store for the best sneakers</p>
+					</div>
+				</div>
+				<ul className="d-flex">
+					<li className='mr-30'>
+						<SlBasket size='18px' color='#9B9B9B' className='logo'/>
+						<span className='ml-10'>1205 $</span>
+					</li>
+					<li>
+						<BiUserCircle size='20px' color='#9B9B9B'/>
+					</li>
+				</ul>
+			</header>
+			<div className="content p-40">
+				<h1>All sneakers</h1>
+				<div className="card">
+					<img src="" alt="" />
+					<p></p>
+					<div>
+						<div>
+							<span>Price:</span>
+							<b>129.99</b>
+						</div>
+						<button>
+							<BsPlus size='11px' color='#D3D3D3'/>
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
